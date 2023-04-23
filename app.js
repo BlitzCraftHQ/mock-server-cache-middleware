770fc27594a88cc7bdd5362ae17d0df6a303e3c7
“Fabian <fabianferno@gmail.com>
2023-04-23 20:29:32 +0530

    Add run script

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const { createClient } = require("redis");
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
-})
+});
 
-redisClient.connect()
+redisClient.connect();
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
-app.get('*', async (req, res) => {
-  let key = req.url.split("?")[0]
-  let data = await redisClient.get(key)
+app.get("*", async (req, res) => {
+  let key = req.url.split("?")[0];
+  let data = await redisClient.get(key);
   if (data) {
-    console.log(`From Redis : ${key}`)
-    res.json(JSON.parse(data))
+    console.log(`From Redis : ${key}`);
+    res.json(JSON.parse(data));
   } else {
-    let data = await fetch(`${process.env.MOCK_URL}${key}`)
-    let jsonData = await data.json()
-    console.log(`Setting in Redis : ${key}`)
-    await redisClient.set(key, JSON.stringify(jsonData))
-    res.json(jsonData)
+    let data = await fetch(`${process.env.MOCK_URL}${key}`);
+    let jsonData = await data.json();
+    console.log(`Setting in Redis : ${key}`);
+    await redisClient.set(key, JSON.stringify(jsonData));
+    res.json(jsonData);
   }
-})
+});
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
-})
+  console.log("http://localhost:5000");
+});
