402e9eab9cd20bb2d6d9bced9f9fd0a3ce514eba
“Fabian <fabianferno@gmail.com>
2023-04-24 23:07:43 +0530

    Use axios

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
+const axios = require("axios");
+
+// Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -11,33 +17,34 @@ const { createClient } = require("redis");
 const redisClient = createClient({
   url: process.env.REDIS_URL,
 });
-
 redisClient.connect();
-
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
-
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
 
 app.get("*", async (req, res) => {
   try {
-    let key = req.url.split("?")[0];
+    let key = req.url;
     let data = await redisClient.get(key);
     if (data) {
-      console.log(`From Redis : ${key}`);
+      console.log(`Got From Redis : ${key}`);
       res.json(JSON.parse(data));
     } else {
-      let data = await fetch(`${process.env.MOCK_URL}${key}`);
-      let jsonData = await data.json();
-      console.log(`Setting in Redis : ${key}`);
-      await redisClient.set(key, JSON.stringify(jsonData));
-      res.json(jsonData);
+      await axios
+        .get(`${process.env.MOCK_URL}${key}`)
+        .then((res) => res.data)
+        .then(async (data) => {
+          console.log(`Setting in Redis : ${key}`);
+          await redisClient.set(key, JSON.stringify(data));
+          res.json(data);
+        })
+        .catch((err) => {
+          res.status(500);
+          res.json(err);
+        });
     }
   } catch (err) {
     console.log(err);
     res.status(500);
-    res.json({ error: err });
+    res.json(err);
   }
 });
 
